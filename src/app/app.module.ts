46b0fc0602cc5cda19471a50db6bf387f094b23c
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2021-03-28 20:34:11 -0400

    Add UsersListComponent to list of imports and declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { UsersListComponent } from './users-list/users-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    UsersListComponent
   ],
   imports: [
     BrowserModule,
