76a90e96b1c16ad37acb4052a89722cda5de306d
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2021-04-01 16:53:49 -0400

    Make app render LandingPageComponent initially and add route for UsersListComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { UsersListComponent } from './users/users-list/users-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+ { path: 'users-list', component: UsersListComponent }, 
+ { path: '', component: LandingPageComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
