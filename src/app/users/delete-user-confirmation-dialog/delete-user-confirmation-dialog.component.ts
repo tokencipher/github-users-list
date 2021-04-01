e7672bc6e9d5b43062b04968b6ad4d8fd796199d
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2021-03-31 21:57:35 -0400

    Add Mat-*/MAT_* imports, inject data object, and define click handler functions

--- src/app/users/delete-user-confirmation-dialog/delete-user-confirmation-dialog.component.ts
+++ src/app/users/delete-user-confirmation-dialog/delete-user-confirmation-dialog.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'app-delete-user-confirmation-dialog',
   templateUrl: './delete-user-confirmation-dialog.component.html',
   styleUrls: ['./delete-user-confirmation-dialog.component.css']
 })
-export class DeleteUserConfirmationDialogComponent implements OnInit {
+export class DeleteUserConfirmationDialogComponent {
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<DeleteUserConfirmationDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: {userID: number}
+  ) { }
 
-  ngOnInit() {
+  onNoClick(): void {
+    this.dialogRef.close(
+      {
+        deleteConfirmed: false,
+        userID: null
+      }
+    );
+  }
+
+  onYesClick(): void {
+    this.dialogRef.close(
+      { 
+        deleteConfirmed: true, 
+        userID: this.data.userID
+      }
+    );
   }
 
 }
