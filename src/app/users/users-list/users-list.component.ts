465149a2e4b43f3a719b85f8aac5b822da4f3ce4
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2021-04-05 17:11:13 -0400

    Comment log statements

--- src/app/users/users-list/users-list.component.ts
+++ src/app/users/users-list/users-list.component.ts
@@ -41,16 +41,18 @@ export class UsersListComponent implements OnInit {
     });
 
     dialogRef.afterClosed().subscribe(result => {
+      /*
       console.log('The dialog was closed');
       console.log('Value of deleteConfirmed: ', result.deleteConfirmed);
       console.log('Value of userID: ', result.userID);
+      */
       this.deleteUser(result.userID);
     });
   }
 
   deleteUser(userID: number) {
     this.users = this.users.filter(x => x.id !== userID);
-    console.log('Updated list of users after delete operation: ', this.users);
+    //console.log('Updated list of users after delete operation: ', this.users);
   }
 
 }
