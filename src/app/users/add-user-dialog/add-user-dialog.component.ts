fb0180da8b6fe2bfa91943453335f0e5a69dd3ee
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2021-04-13 19:38:36 -0400

    Initialize userForm property in constructor

--- src/app/users/add-user-dialog/add-user-dialog.component.ts
+++ src/app/users/add-user-dialog/add-user-dialog.component.ts
@@ -2,34 +2,37 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { MatDialogRef } from '@angular/material';
 
+
 @Component({
   selector: 'app-add-user-dialog',
   templateUrl: './add-user-dialog.component.html',
   styleUrls: ['./add-user-dialog.component.css']
 })
 export class AddUserDialogComponent implements OnInit {
-  userForm = new FormGroup({
-    login: new FormControl(''),
-    id: new FormControl(''),
-    node_id: new FormControl(''),
-    avatar_url: new FormControl(''),
-    gravatar_id: new FormControl(''),
-    url: new FormControl(''),
-    html_url: new FormControl(''),
-    followers_url: new FormControl(''),
-    following_url: new FormControl(''),
-    gists_url: new FormControl(''),
-    starred_url: new FormControl(''),
-    subscriptions_url: new FormControl(''),
-    organizations_url: new FormControl(''),
-    repos_url: new FormControl(''),
-    events_url: new FormControl(''),
-    received_events_url: new FormControl(''),
-    type: new FormControl(''),
-    site_admin: new FormControl('')
-  });
+  userForm: FormGroup;
 
-  constructor(public dialogRef: MatDialogRef<AddUserDialogComponent>) { }
+  constructor(public dialogRef: MatDialogRef<AddUserDialogComponent>) { 
+    this.userForm = new FormGroup({
+      login: new FormControl(''),
+      id: new FormControl(''),
+      node_id: new FormControl(''),
+      avatar_url: new FormControl(''),
+      gravatar_id: new FormControl(''),
+      url: new FormControl(''),
+      html_url: new FormControl(''),
+      followers_url: new FormControl(''),
+      following_url: new FormControl(''),
+      gists_url: new FormControl(''),
+      starred_url: new FormControl(''),
+      subscriptions_url: new FormControl(''),
+      organizations_url: new FormControl(''),
+      repos_url: new FormControl(''),
+      events_url: new FormControl(''),
+      received_events_url: new FormControl(''),
+      type: new FormControl(''),
+      site_admin: new FormControl('')
+    });
+  }
 
   ngOnInit() {
   }
