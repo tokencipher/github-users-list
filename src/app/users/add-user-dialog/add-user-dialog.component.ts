28965f1bf4b7cff21c91eec652a9d678c60137af
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2021-04-13 19:49:03 -0400

    Create helper 'User' class to enact Model Driven Reactive form

--- src/app/users/add-user-dialog/add-user-dialog.component.ts
+++ src/app/users/add-user-dialog/add-user-dialog.component.ts
@@ -2,6 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { MatDialogRef } from '@angular/material';
 
+class User {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+
+  constructor(init?: Partial<User>) {
+    Object.assign(this, init);
+  }
+}
+
 
 @Component({
   selector: 'app-add-user-dialog',
