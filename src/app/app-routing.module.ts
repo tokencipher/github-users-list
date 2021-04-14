d1149aa695911a4ed6c8e10f4706e1d439ff6af5
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2021-04-14 11:36:50 -0400

    Add route for PageNotFound component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { UsersListComponent } from './users/users-list/users-list.component';
 
 const routes: Routes = [ 
  { path: 'users-list', component: UsersListComponent }, 
- { path: '', component: LandingPageComponent, pathMatch: 'full' }
+ { path: '', component: LandingPageComponent, pathMatch: 'full' },
+ { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
