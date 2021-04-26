00c24daaa7b07dcccb38fd226d4fedd6b3f8511f
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2021-04-26 15:56:44 -0400

    Implement auth service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../users/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private authAPI: string = '/api/auth';
+  private accessToken: string;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createUser(user: User) {
+    return this.http.post(`${this.authAPI}/create`, {user});
+  } 
+
+  updateUser(user: Partial<User>) {
+    return this.http.put(`${this.authAPI}/update/${user.user_id}`, {user});
+  }
+
+  deleteUser(user: User) {
+    return this.http.delete(`${this.authAPI}/delete/${user.user_id}`);
+  }
+
+  login(user: Partial<User>) {
+    return this.http.post(`${this.authAPI}/login`, user);
+  }
+
+
 }
