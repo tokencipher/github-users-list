fe299822ba61e5b283850e564c4b2d9fd4c2ad4f
Bryan Thomas <bryan.thomas.nyc@gmail.com>
2021-04-30 11:24:12 -0400

    Implement setSession() method

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../users/user';
+import { pipe } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import * as moment from 'moment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +13,7 @@ export class AuthService {
   private authAPI: string = '/api/auth';
   accessToken: string;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   createUser(user: User) {
     return this.http.post(`${this.authAPI}/create`, {user});
@@ -23,8 +27,21 @@ export class AuthService {
     return this.http.delete(`${this.authAPI}/delete/${user.user_id}`);
   }
 
+  private setSession(payload) {
+    const expiry = moment().add(payload.expiresIn, 'second');
+
+    localStorage.setItem('access_token', payload.access_token);
+    localStorage.setItem("expiry", JSON.stringify(expiry.valueOf()));
+  }
+
   login(user: Partial<User>) {
-    return this.http.post(`${this.authAPI}/login`, user);
+    return this.http.post(`${this.authAPI}/login`, user)
+      .pipe(
+        map((res: any) => this.setSession)
+      );
+    // Can possibly call shareReplay() to prevent the receiver of 
+    // this Observable from accidentally triggering multiple POST
+    // requests due to multiple subscriptions.
   }
 
 
